67f72dc8b1a0e5ddfa9b7a5387fab500481b6488
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2018-09-09 19:12:13 +0900

    Fetch a 5 minutes ago image as a fallback image
    If an amesh rain gif is not found,
    instead fetch a 5 minutes ago image as a fallback image.

--- spec/lita/handlers/amesh/downloader_spec.rb
+++ spec/lita/handlers/amesh/downloader_spec.rb
@@ -3,9 +3,9 @@
 require 'spec_helper'
 
 describe Lita::Handlers::Amesh::Downloader do
-  describe '#rain_gif_path' do
+  describe '#target_time' do
     let(:downloader) { described_class.new }
-    subject { downloader.send(:rain_gif_path) }
+    subject { downloader.send(:target_time) }
 
     before do
       allow(Time).to receive(:now).and_return(current_time)
@@ -15,7 +15,7 @@ describe Lita::Handlers::Amesh::Downloader do
       let(:current_time) { Time.new(2018, 1, 1, 0, 0) }
 
       it 'returns an url path based on a current time' do
-        expect(subject).to eq('mesh/000/201801010000.gif')
+        expect(subject).to eq(current_time)
       end
     end
 
@@ -23,8 +23,18 @@ describe Lita::Handlers::Amesh::Downloader do
       let(:current_time) { Time.new(2018, 1, 1, 0, 1) }
 
       it 'returns an url path based on the time that is divisible by 5 before a current time' do
-        expect(subject).to eq('mesh/000/201801010000.gif')
+        expect(subject).to eq(Time.new(2018, 1, 1, 0, 0))
       end
+    end
+  end
+
+  describe '#five_minutes_ago' do
+    let(:downloader) { described_class.new }
+
+    it 'returns a 5 minutes ago image of given time' do
+      time = Time.new(2018, 1, 1, 0, 0)
+      actual = downloader.send(:five_minutes_ago, time)
+      expect(actual).to eq(Time.new(2017, 12, 31, 23, 55))
     end
   end
 end
